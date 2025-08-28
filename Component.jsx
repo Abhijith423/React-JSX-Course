1007c48ab8da655d2062f4f3d216023ed21e6955
Abhijith <abhijithemes@gmail.com>
2025-08-28 23:13:06 +0530

    Component.jsx

---
+++ Component.jsx
@@ -0,0 +1,25 @@
+components in jsx simply we can open a jsx file and type rafce then basic syntax can be displayed
+
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+
+function App() {
+  
+  return (
+
+ < >
+<Header></Header>
+<Home></Home>                => these are components in react
+<Home></Home>
+<Home></Home>
+</>
+        
+  );
+}
+
+export default App
