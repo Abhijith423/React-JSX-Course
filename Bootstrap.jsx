ce74ac8cf2d27f24c4dfbe6dd5defd03eda778bb
Abhijith <abhijithemes@gmail.com>
2025-08-29 15:00:35 +0530

    Bootstrap.jsx

---
+++ Bootstrap.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import {Button, Table, Alert,Card,Nav} from 'react-bootstrap' 
+
+const Home = () => {
+  return (
+    <>
+    <div>Home Component</div>
+    <Button variant="success">Primary</Button>
+    <br />
+      <Button variant="warning">Warning</Button>
+      <br />
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+      {[
+        'primary',
+        ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+      <br />
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://picsum.photos/seed/picsum/200/300" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+     <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+
+    </>
+    
+  )
+}
+
+export default Home
